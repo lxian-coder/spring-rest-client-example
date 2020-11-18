64b2e6842d4503550a3e7b3504023441d2f47fcf
darcyxian <darcyxianau@gmail.com>
2020-11-18 21:56:23 +1100

    create POJOs

--- src/main/java/guru/springwork/springrestclientexamples/SpringRestClientExamplesApplication.java
+++ src/main/java/guru/springwork/springrestclientexamples/SpringRestClientExamplesApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.springrestclientexamples;
+package guru.springwork.springrestclientexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/guru/springwork/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
+++ src/test/java/guru/springwork/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.springrestclientexamples;
+package guru.springwork.springrestclientexamples;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
